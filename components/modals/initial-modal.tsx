da7dbe1effa2d58b507886a6d20d41cfd1bcd385
Ahmed Abrar <abrahmed@gmail.com>
2023-09-06 22:28:45 +0100

    added tooltip and server Api routes

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import {
   Dialog,
@@ -25,6 +26,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
 import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -37,6 +39,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -53,7 +57,14 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      await axios.post("/api/servers", values);
+      form.reset();
+      router.refresh();
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   if (!isMounted) {
